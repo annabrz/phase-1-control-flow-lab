b250d50c630c6b7b419425a480a7f3539f5c0c75
AnnaAyvazyan <ayvazyananna@yahoo.com>
2022-06-30 17:36:27 -0700

    commit tree 284adee31cd860eca1729813dc34aafe27759add

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let price
+
+  if (feet <= 400) {
+     price = "This one is on me!"
+ }
+ else if (feet<2000 ){
+  price = "That will be twenty bucks."
+ }
+ else if (feet<2500){
+    price = "I will gladly take your thirty bucks."
+ }
+ else if (feet > 2500){
+   price = "No can do."
+ }
+
+
+ return price
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+if (city == 'NYC'){
+  return "Ok, sounds good."
+}
+else {
+  return "No go."
+}
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+switch (tip){
+  case 'generous':
+  return('Thank you so much.')
+  break;
+
+  case 'not as generous':
+  return('Thank you.')
+  break;
+
+  default: 
+  return('Bye.')
+  break;
+
+
+}
+
 }
